b81acf7b793f0d18fcaa666c204ec40a7f66f354
yedf <dongfuye@163.com>
2015-07-22 15:52:08 +0800

    python2 supported

--- oss_sample.py
+++ oss_sample.py
@@ -2,20 +2,23 @@ import base64
 import hmac
 from hashlib import sha1
 import time
-import urllib.request
+try:
+    import urllib.request as urllib
+except ImportError:
+    import urllib2 as urllib
 
-accessKeyId = 'xxxxxx'
-accessKeySecret = 'xxxxxxxx'
+accessKeyId = 'xxxxx'
+accessKeySecret = 'xxxxx'
 
-# 这里使用了在url进行签名的方式
+# use signature in url
 def _oss_file_url(method, bucket, filename, content_type):
     now = int(time.time())
-    expire = now - (now % 1800) + 3600 #避免每个时刻url都不一样
+    expire = now - (now % 1800) + 3600 # expire will not different every second
     tosign = "%s\n\n\n%d\n/%s/%s" % (method, expire, bucket, filename)
     if method == 'PUT' or method == 'POST':
         tosign = "%s\n\n%s\n%d\n/%s/%s" % (method, content_type, expire, bucket, filename)
     h = hmac.new(accessKeySecret.encode(), tosign.encode(), sha1)
-    sign = urllib.request.quote(base64.encodebytes(h.digest()).strip())
+    sign = urllib.quote(base64.encodestring(h.digest()).strip())
     return 'http://%s.oss-cn-hangzhou.aliyuncs.com/%s?OSSAccessKeyId=%s&Expires=%d&Signature=%s' % (
         bucket, filename, accessKeyId, expire, sign
     )
@@ -26,14 +29,14 @@ def get_file_url(bucket, filename):
 
 def http_put(bucket, filename, cont, content_type):
     url = _oss_file_url('PUT', bucket, filename, content_type)
-    req = urllib.request.Request(url, cont)
+    req = urllib.Request(url, cont)
     req.get_method = lambda: 'PUT'
     req.add_header('content-type', content_type)
     try:
-        return urllib.request.urlopen(req)
-    except urllib.request.HTTPError as e:
+        return urllib.urlopen(req)
+    except urllib.HTTPError as e:
         print(e)
 
 http_put('fast-loan', 'mytestkey', b'sample value', 'text/plain')
 url = get_file_url('fast-loan', 'mytestkey')
-print(urllib.request.urlopen(url).read())
+print(urllib.urlopen(url).read())
